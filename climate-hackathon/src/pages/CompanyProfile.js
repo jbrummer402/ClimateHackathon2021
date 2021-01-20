f39af1ed72c66e5145b6155bcb2df03bdd2eae16
Jack Brummer <jbrummer@stevens.edu>
2021-01-20 13:52:37 -0500

    Adding inputs for company profile

--- climate-hackathon/src/pages/CompanyProfile.js
+++ climate-hackathon/src/pages/CompanyProfile.js
@@ -1,11 +1,30 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 function CompanyProfile(){
     return (
-        <div>
-            test
-        </div>
+        <>
+            <form>
+                <label /> What is the name of your business?
+                <input type="text" name="name" />
+            </form>
+            <Dropdown>
+
+            </Dropdown>
+            <form>
+                <label /> What will be the business's admin email?
+                <input type="text" name="name" />
+            </form>
+            <form>
+                <label /> What is/are the name(s) of the business owner(s)?
+                <input type="text" name="name" />
+            </form>
+            <form>
+                <label /> Please provide a phone number for this business.
+                <input type="text" name="name" />
+            </form>
+        </>
     )
 
 }
