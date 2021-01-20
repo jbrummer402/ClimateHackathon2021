05bdb0bac6928d900ad252431512e014495981cc
Jack Brummer <jbrummer@stevens.edu>
2021-01-20 13:15:15 -0500

    working on company profile page

---
+++ climate-hackathon/src/pages/CompanyProfile.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+function CompanyProfile(){
+    return (
+        <div>
+            test
+        </div>
+    )
+
+}
+
+export default CompanyProfile;
